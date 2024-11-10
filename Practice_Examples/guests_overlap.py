9e093562b7234651fb076a0284f4a2550656f807
DriesSmit <dries.epos@gmail.com>
2024-11-10 15:29:54 +0200

    feat: save progress

--- Practice_Examples/guests_overlap.py
+++ Practice_Examples/guests_overlap.py
@@ -1,1 +1,40 @@
-print("ls")
+"""
+Link: https://www.geeksforgeeks.org/find-the-point-where-maximum-intervals-overlap/?ref=header_outind
+Consider a big party where a log register for guest’s entry and exit times is maintained. Find the time at which there are maximum guests in 
+the party. Note that entries in register are not in any order.
+Example : 
+
+Input: arrl[] = {1, 2, 9, 5, 5}
+       exit[] = {4, 5, 12, 9, 12}
+First guest in array arrives at 1 and leaves at 4, 
+second guest arrives at 2 and leaves at 5, and so on.
+
+Output: 5
+There are maximum 3 guests at time 5.  
+"""
+
+def maximumGuests(arrl, exit):
+    # Insight 1: We don't care which guest leaves. Therefore we do not have to respect the arrl, exit ordering
+    arrl.sort() # 1, 2, 5, 5, 9
+    exit.sort() # 4, 5, 9, 12, 12
+    # 1, 2, 1, 2, 3, 2, 1, 2, 1, 0
+
+    guests = max_guests = 1
+    a_i = 1
+    e_i = 0
+    while a_i < len(arrl) and e_i < len(exit):
+        if arrl[a_i] <= exit[e_i]:
+            guests += 1
+            a_i += 1
+            if guests > max_guests:
+                max_guests = guests
+        else:
+            guests -= 1
+            e_i += 1
+    return max_guests
+
+
+
+if __name__ == "__main__":
+    answer = maximumGuests([1, 2, 9, 5, 5], [4, 5, 12, 9, 12])
+    assert answer == 3, f"Incorrect answer: {answer}. Expected 3."
--- README.md
+++ README.md
@@ -4,5 +4,6 @@ A collection of leetcode and other coding exercise solutions.
 # General learning priciples
 1. Attempt a new LeetCode problem that is slightly more challenging than your current skill level.
 2. If you get stuck, do not look at hints, categories, or solutions. Instead, identify specific areas where you need further study, focus on those, then return to the problem and try solving it again.
-3. If you’re still unsure how to progress, skip the problem and revisit it after more practice.
+3. If you’re still unsure how to progress, look at simular questions and try to solve them first.
+4. Thereafter try looking at hints and categories.
 4. If you solve the problem, review the top solutions. Reflect on how others approached the problem and think about ways to incorporate their methods into your problem-solving approach to achieve similar results in the future.
